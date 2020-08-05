f609ad684d22113ee4a21afa2388c9dcac2154c2
Minvydas <ml622@kent.ac.uk>
2020-08-05 18:01:14 +0100

    Code refactored

--- uutils.js
+++ uutils.js
@@ -1,36 +1,50 @@
-function UU(selector) {
-    const self = {
-        element: document.querySelector(selector),
-        html: () => self.element,
-        on: (event, callback) => {
-            self.element.addEventListener(event, callback);
-        },
-        style: new Proxy({}, {
-            get(target, property) {
-                if (self.element.style.hasOwnProperty(property)) {
-                    return argument => {
-                        if (argument) {
-                            self.element.style[property] = argument;
-                        } else {
-                            return self.element.style[property];
+class UU {
+    static select(selector){
+        const element = document.querySelector(selector);
+        const options = {
+            on: (event, callback) => {
+                element.addEventListener(event, callback);
+            },
+            style: new Proxy({}, {
+                get(target, property) {
+                    if (element.style.hasOwnProperty(property)) {
+                        return argument => {
+                            if (argument) {
+                                element.style[property] = argument;
+                            } else {
+                                return element.style[property];
+                            }
                         }
                     }
+                    throw Error("Property \"" + property + "\" doesn't exist");
                 }
-                throw Error("Property \"" + property + "\" doesn't exist");
+            }),
+        };
+        return options;
+    }
+    static ajax = () => {
+        const xhr = new XMLHttpRequest();
+        const options = {
+            get:(url, callback) => {
+                xhr.open('GET', url, true);
+                xhr.onload = () => {
+                    if(xhr.status === 200){
+                        callback(JSON.parse(xhr.responseText));
+                    }else{
+                        callback('Error: ' + xhr.status);
+                    };
+                }
+                xhr.send();
+            },
+            post:(url, data, callback) => {
+                xhr.open('POST', url, true);
+                xhr.setRequestHeader('Content-Type', 'application/json');
+                xhr.onload = () => {
+                    console.log(xhr.responseText);
+                }
+                xhr.send(JSON.stringify(data));
             }
-        }),
-        ajax: (method, url, async) => {
-            const xhr = new XMLHttpRequest();
-            let response = ""; 
-            xhr.open(method, url, async);
-            xhr.onload = function (){
-                if(this.status === 200){
-                   response = JSON.parse(this.responseText);
-                };
-            }
-            xhr.send();
-            return response;
         }
-    };
-    return self;
+        return options;
+    }
 }
